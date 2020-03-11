37ab3032530572d67b99e0a1e43fc0bd9498b5d0
omid javaheri <omid.javaheri66@gmail.com>
2020-03-11 15:43:19 +0330

    ...

--- src/main/java/ir/hitelecom/accounting/repositories/stock/ProductSizeRepository.java
+++ src/main/java/ir/hitelecom/accounting/repositories/stock/ProductSizeRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public interface ProductSizeRepository extends CrudRepository<ProductSize,Long> {
 
-    @Query(value = "select s.id , s.value, ps.count from sizes as s left join product_size as ps on ps.size_id = s.id and ps.product_id = :productId", nativeQuery = true)
+    @Query(value = "select s.id , s.value, ps.count, ps.id as psid from sizes as s left join product_size as ps on ps.size_id = s.id and ps.product_id = :productId", nativeQuery = true)
     List<Object[]> getSizes(@Param("productId") Long productId);
 
     void deleteByProduct(Product product);
--- src/main/java/ir/hitelecom/accounting/services/stock/ProductSizeService.java
+++ src/main/java/ir/hitelecom/accounting/services/stock/ProductSizeService.java
@@ -28,6 +28,7 @@ public class ProductSizeService extends BaseService {
         List<Object[]> list = productSizeRepository.getSizes(productId != null ? productId : 0);
         list.forEach(elem -> {
             ProductSize productSize = new ProductSize();
+            productSize.setId(((BigInteger) elem[3]).longValue());
             Size size = new Size();
             size.setId(((BigInteger) elem[0]).longValue());
             size.setValue((String) elem[1]);
