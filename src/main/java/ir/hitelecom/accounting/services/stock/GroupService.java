301b2c4c48720e3ef743c106ceb8d436307a88af
omid javaheri <omid.javaheri66@gmail.com>
2020-03-17 16:19:38 +0330

    ...

--- src/main/java/ir/hitelecom/accounting/services/stock/GroupService.java
+++ src/main/java/ir/hitelecom/accounting/services/stock/GroupService.java
@@ -1,8 +1,11 @@
 package ir.hitelecom.accounting.services.stock;
 
+import ir.hitelecom.accounting.entities.User;
 import ir.hitelecom.accounting.entities.stock.Group;
+import ir.hitelecom.accounting.repositories.UserRepository;
 import ir.hitelecom.accounting.repositories.stock.GroupRepository;
 import ir.hitelecom.accounting.services.BaseService;
+import ir.hitelecom.accounting.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +18,17 @@ public class GroupService extends BaseService {
 
     @Autowired
     private GroupRepository groupRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     public List<Group> fetchAll(){
-        return groupRepository.findByUsername(getLoggedInUsername());
+        User user = userRepository.findByUsername(getLoggedInUsername());
+        return groupRepository.findByUsername(user.getParent().getUsername());
     }
 
     public Group saveOrUpdate(Group group) {
-        group.setUsername(getLoggedInUsername());
+        User user = userRepository.findByUsername(getLoggedInUsername());
+        group.setUsername(user.getParent().getUsername());
         return groupRepository.save(group);
     }
 
